d1a3aba60e546ca74c217bbbab0d4b8eccb1fcce
Christopher Hernandez <cerberean661@gmail.com>
2019-11-11 15:08:15 -0800

    first test passed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -3,3 +3,8 @@ import React from 'react';
 import Display from './Display.js';
 import { render } from '@testing-library/react';
 
+describe('<Display />', () => {
+    it('render w/o crash', () => {
+        render(<Display />);
+    })
+})
