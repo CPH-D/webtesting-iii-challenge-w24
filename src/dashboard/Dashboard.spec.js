84b13080220e06103c2bb7727ac53508b2611347
Christopher Hernandez <cerberean661@gmail.com>
2019-11-12 00:16:45 -0800

    Dashboard render test passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Test away
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react/pure';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard', () => {
+    beforeEach(cleanup);
+    it('render w/o crash', () => {
+        render(<Dashboard />);
+    });
+});
