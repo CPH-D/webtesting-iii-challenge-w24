c9fe54a0eb132cd0fc2c3294e7c7f2a49ad85c6c
Christopher Hernandez <cerberean661@gmail.com>
2019-11-11 22:04:14 -0800

    closed/unlocked & closed/locked tests passed

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,5 +6,25 @@ import { render } from '@testing-library/react';
 describe('<Display />', () => {
     it('render w/o crash', () => {
         render(<Display />);
-    })
-})
+    });
+
+    it('open & unlocked test', () => {
+        // testing to see if it is true that closed/locked is false
+        const { getByText } = render(<Display closed={false} locked={false}/>);
+        getByText(/unlocked/i);
+        getByText(/open/i);
+    });
+
+    it('closed & unlocked test', () => {
+        const { getByText } = render(<Display closed={true} locked={false} />);
+        getByText(/unlocked/i);
+        getByText(/closed/i);
+    });
+    
+    it('closed & locked test', () => {
+        const { getByText } = render(<Display closed={true} locked={true} />);
+        // adding '^' and '$' expressions to /locked/i to target EXACTLY the word locked, because locked can be mixed with unlocked
+        getByText(/^locked$/i);
+        getByText(/closed/i);
+    });
+});
