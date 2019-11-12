2a1edff0ffa015a4be6e34b2b3d710a5269926da
Christopher Hernandez <cerberean661@gmail.com>
2019-11-12 00:09:00 -0800

    close & lock button test success

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import Controls from './Controls';
 
 afterEach(cleanup);
@@ -9,4 +9,28 @@ describe('<Controls />', () => {
     it('render w/o crash', () => {
         render(<Controls />);
     });
+
+    it('open and unlocked', () => {
+        const closeCheck = jest.fn();
+        const lockCheck = jest.fn();
+        const { getByText } = render(<Controls closed={false}
+                                                locked={false}
+                                                toggleClosed={closeCheck}
+                                                toggleLocked={lockCheck}
+                                                />);
+        const closeButton = getByText(/close gate/i);
+        const lockButton = getByText(/lock gate/i);
+
+        // verifying disabled button
+        expect(closeButton.disabled).toBeFalsy();
+        expect(lockButton.disabled).toBeTruthy();
+
+        // verifying button click status
+        fireEvent.click(closeButton);
+        expect(closeCheck).toBeCalled();
+
+        fireEvent.click(lockButton);
+        expect(lockCheck).not.toBeCalled();
+    });
 });
+
