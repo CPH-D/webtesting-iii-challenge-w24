3b0ff1254fff835d1a67d0241f65770daa8f0b3f
Christopher Hernandez <cerberean661@gmail.com>
2019-11-12 00:31:43 -0800

    dashboard tests pass

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -15,12 +15,64 @@ describe('<Dashboard /> state transitions', () => {
     const { getByText } = render (<Dashboard />);
     it('default state open & unlocked', () => {
         
+        // check display text
         getByText(/^open$/i);
         getByText(/^unlocked$/i);
 
+        // check button text
         getByText(/^lock gate$/i);
         getByText(/^close gate$/i);
     });
 
-    
+    it('open & unlocked to closed & unlocked', () => {        
+        const closeButton = getByText(/^close gate$/i);
+        fireEvent.click(closeButton);
+
+        // check display text
+        getByText(/^closed$/i);
+        getByText(/^unlocked$/i);
+
+        // check button text
+        getByText(/^lock gate$/i);
+        getByText(/^open gate$/i);
+    });
+
+    it('closed & unlocked to closed & locked', () => {        
+        const lockButton = getByText(/^lock gate$/i);
+        fireEvent.click(lockButton);
+
+        // check display text
+        getByText(/^closed$/i);
+        getByText(/^locked$/i);
+
+        // check button text
+        getByText(/^unlock gate$/i);
+        getByText(/^open gate$/i);
+    });
+
+    it('closed & locked to closed & unlocked', () => {        
+        const lockButton = getByText(/^unlock gate$/i);
+        fireEvent.click(lockButton);
+
+        // check display text
+        getByText(/^closed$/i);
+        getByText(/^unlocked$/i);
+
+        // check button text
+        getByText(/^lock gate$/i);
+        getByText(/^open gate$/i);
+    });
+
+    it('closed & unlocked to open & unlocked', () => {        
+        const openButton = getByText(/^open gate$/i);
+        fireEvent.click(openButton);
+
+        // check display text
+        getByText(/^open$/i);
+        getByText(/^unlocked$/i);
+
+        // check button text
+        getByText(/^lock gate$/i);
+        getByText(/^close gate$/i);
+    });
 });
